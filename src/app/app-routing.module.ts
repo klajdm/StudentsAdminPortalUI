a2d8aff10b13f59f4edb509527b07827540d2eb1
klajdm <klajdimurati3@gmail.com>
2023-10-15 14:27:37 +0200

    changes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './component/students/students.component';
+import { ViewStudentComponent } from './component/students/view-student/view-student.component';
+
+const routes: Routes = [
+  {
+    path: '', component: StudentsComponent
+  },
+  {
+    path: 'students', component: StudentsComponent
+  },
+  {
+    path: 'students/:id', component: ViewStudentComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/component/students/view-student/view-student.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Gender } from 'src/app/models/ui-models/gender.model';
+import { Student } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/services/gender.service';
+import { StudentService } from 'src/app/services/student.service';
+
+@Component({
+  selector: 'app-view-student',
+  templateUrl: './view-student.component.html',
+  styleUrls: ['./view-student.component.css']
+})
+export class ViewStudentComponent implements OnInit {
+
+  studentId: string | null | undefined;
+
+  student: Student = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    email: '',
+    mobile: 0,
+    genderId: '',
+    profileImagerl: '',
+    gender: {
+      id: '',
+      description: ''
+    },
+    address: {
+      id: '',
+      physicalAddress: '',
+      postalAddres: ''
+    }
+  }
+
+  genderList: Gender[] = [];
+
+  constructor(
+    private readonly studentService: StudentService,
+    private readonly genderService: GenderService,
+    private readonly route: ActivatedRoute,
+    private snackbar: MatSnackBar,
+    private router: Router
+  ) {
+
+  }
+
+
+  ngOnInit(): void {
+
+    this.route.paramMap.subscribe(
+      (params) => {
+        this.studentId = params.get('id');
+
+        if (this.studentId) {
+          this.studentService.getStudent(this.studentId)
+            .subscribe(
+              (successResponse) => {
+                this.student = successResponse;
+              }
+            );
+
+          this.genderService.getGenderList()
+            .subscribe(
+              (successResponse) => {
+                this.genderList = successResponse;
+              }
+            )
+        }
+      }
+    )
+
+  }
+
+  onUpdate(): void {
+    // Call Student Service to Update Student
+    this.studentService.updateStudent(this.student.id, this.student)
+      .subscribe(
+        (successResponse) => {
+          // Show a notification
+          this.snackbar.open('Student updated successfully', undefined, {
+            duration: 2000
+          })
+        },
+        (errorResponse) => {
+          // Log it
+        }
+      )
+
+  }
+
+  onDelete(): void {
+    this.studentService.deleteStudent(this.student.id)
+      .subscribe(
+        (successResponse) => {
+          this.snackbar.open('Student deleted successfully', undefined, {
+            duration: 2000
+          });
+
+          setTimeout(() => {
+            this.router.navigateByUrl('students');
+          }, 2000);
+
+        },
+        (errorResponse) => {
+          //Log
+
+        }
+      )
+  }
+
+}
